6aa85dad9ede8d64e224fbbbe83f4db8519dd1de
[Gabriel Rodrigues Leite] <k.cogabriel@gmail.com>
2021-10-19 20:22:41 -0300

    teste IV implementado

--- tests/magic.test.js
+++ tests/magic.test.js
@@ -11,13 +11,14 @@ const expected = {
 
 describe(' Testa a função getMagicCard', () => {
   it('Deve retornar um objeto com as propriedades esperadas', async () => {
-    expect.assertions(2);
+    expect.assertions(3);
     const getMagicCardSimulator = trybeSimulator(magic, 'getMagicCard');
     const card = await getMagicCardSimulator('130550');
 
     // implemente seus testes aqui    
     expect(expected).toEqual(card);               // I & II
     expect(typeof card.types).toEqual(typeof []); // teste III
+    expect(card.subtypes.length).toEqual(2);     // teste IV
    
   });
 });
